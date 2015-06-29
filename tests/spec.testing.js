dd07a2f2933fb780421c03595098f33bae6f732d
Mark Kimball <parkskier@gmail.com>
2015-06-29 09:45:16 -0400

    cleaned up testing, used beforeEach to eliminate repetition

--- tests/spec.testing.js
+++ tests/spec.testing.js
@@ -1,7 +1,12 @@
 describe('Constructors', function(){
   describe('Runner Constructor', function(){
+    beforeEach(function(){
+      this.defaultRunner = new page.Runner();
+      this.defaultShoes = new page.Shoes("Chucks", 10);
+      this.defaultTraining = new page.Training("Long Run", 2, 5, 3);
+      this.defaultCourse = new page.Course("Marathon", "asphalt", "26.2 miles", 2620, 3);
+    })
     it('should use default values if no arguments passed', function(){
-      before(this.defaultRunner = new page.Runner());
       expect(this.defaultRunner.name).to.equal("Johnny Rocket");
       expect(this.defaultRunner.color).to.equal("black");
       expect(this.defaultRunner.speed).to.equal(10);
@@ -11,15 +16,12 @@ describe('Constructors', function(){
       expect(this.defaultRunner.shoes.speed).to.equal(10);
     });
     it('should be an object', function(){
-      before(this.defaultRunner = new page.Runner());
       expect(this.defaultRunner).to.be.an('Object');
     })
     it('should be instance of Runner', function(){
-      before(this.defaultRunner = new page.Runner());
       expect(this.defaultRunner).to.be.instanceOf(page.Runner);
     })
     it('should have the proper types', function(){
-      before(this.defaultRunner = new page.Runner());
       expect(this.defaultRunner.name).to.be.a("String");
       expect(this.defaultRunner.color).to.be.a("String");
       expect(this.defaultRunner.speed).to.be.a("Number");
@@ -29,76 +31,56 @@ describe('Constructors', function(){
       expect(this.defaultRunner.shoes.speed).to.be.a("Number");
     })
     it('getShoes is a function of Runner constructor', function(){
-      before(this.defaultRunner = new page.Runner());
       expect(this.defaultRunner.getShoes).is.a('Function');
     })
     it('should be able to use getShoes function and shoes update', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultShoes = new page.Shoes("Chucks", 10));
       before(this.defaultRunner.getShoes(this.defaultShoes));
       expect(this.defaultRunner.shoes.name).to.equal("Chucks");
       expect(this.defaultRunner.shoes.speed).to.equal(10);
     })
     it('train is a function of Runner constructor', function(){
-      before(this.defaultRunner = new page.Runner());
       expect(this.defaultRunner.train).is.a('Function');
     })
     it('should call train once when executed', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultTraining = new page.Training("Long Run", 2, 5, 31));
       sinon.spy(this.defaultRunner, 'train');
       this.defaultRunner.train(this.defaultTraining);
       expect(this.defaultRunner.train).to.have.been.calledOnce;
     })
     it('should call train with correct training', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultTraining = new page.Training("Long Run", 2, 5, 31));
       var spy = sinon.spy(this.defaultRunner, 'train');
       this.defaultRunner.train(this.defaultTraining);
       expect(spy.calledWith(this.defaultTraining)).to.be.ok;
     })
     it('should get injured in training when injury risk of training is greater than random number', function(){
       before(sinon.stub(Math, "random", function(){return 1;}));
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultTraining = new page.Training("Long Run", 2, 5, 31));
+      before(this.defaultTraining.injuryRisk = 31);
       before(this.defaultRunner.train(this.defaultTraining));
       expect(this.defaultRunner.speed).to.equal(8);
       expect(this.defaultRunner.endurance).to.equal(5);
     })
     it('should increase stats in training when injury risk of training is less than random number', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultTraining = new page.Training("Long Run", 2, 5, 3));
       before(this.defaultRunner.train(this.defaultTraining));
       expect(this.defaultRunner.speed).to.equal(12);
       expect(this.defaultRunner.endurance).to.equal(15);
       after(Math.random.restore());
     })
     it('should execute new template twice when train function executed', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultTraining = new page.Training("Long Run", 2, 5, 3));
       before(this.defaultRunner.train(this.defaultTraining));
       expect(page.loadTemplate).twice;
     })
     it('runRace is a function of Runner constructor', function(){
-      before(this.defaultRunner = new page.Runner());
       expect(this.defaultRunner.runRace).is.a('Function');
     })
     it('should execute new template when runRace function executed', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultCourse = new page.Course("Marathon", "asphalt", "26.2 miles", 2620, 3));
       before(this.defaultRunner.runRace(this.defaultCourse));
       expect(page.loadTemplate).twice;
     })
     it('should call runRace once when executed', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultCourse = new page.Course("Marathon", "asphalt", "26.2 miles", 2620, 3));
       sinon.spy(this.defaultRunner, 'runRace');
       this.defaultRunner.runRace(this.defaultCourse);
       expect(this.defaultRunner.runRace).to.have.been.calledOnce;
     })
     it('should call runRace with correct course', function(){
-      before(this.defaultRunner = new page.Runner());
-      before(this.defaultCourse = new page.Course("Marathon", "asphalt", "26.2 miles", 2620, 3));
       var spy = sinon.spy(this.defaultRunner, 'runRace');
       this.defaultRunner.runRace(this.defaultCourse);
       expect(spy.calledWith(this.defaultCourse)).to.be.ok;
@@ -106,21 +88,20 @@ describe('Constructors', function(){
   })
 
   describe('Shoes Constructor', function(){
+    beforeEach(function(){
+      this.defaultShoes = new page.Shoes();
+    })
     it('should use default values if no arguments passed', function(){
-      before(this.defaultShoes = new page.Shoes());
       expect(this.defaultShoes.name).to.equal("barefoot");
       expect(this.defaultShoes.speed).to.equal(10);
     })
     it('should be an object', function(){
-      before(this.defaultShoes = new page.Shoes());
       expect(this.defaultShoes).to.be.an('Object');
     })
     it('should be instance of Shoes', function(){
-      before(this.defaultShoes = new page.Shoes());
       expect(this.defaultShoes).to.be.instanceOf(page.Shoes);
     })
     it('should have the proper types', function(){
-      before(this.defaultShoes = new page.Shoes());
       expect(this.defaultShoes.name).to.be.a("String");
       expect(this.defaultShoes.speed).to.be.a("Number");
     })
